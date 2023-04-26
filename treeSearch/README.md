e7e5209be45b35dd253f4ed838d2d6ed8ecc9e1e
Joel Smith <joelsmith2004@gmail.com>
2023-04-26 00:30:44 +0000

    26 Apr 2023, 08:30

---
+++ treeSearch/README.md
@@ -0,0 +1,12 @@
+## Getting Started
+
+Welcome, this project is part of TreeSearch lab week 6 for CITS2200
+
+## Folder Structure
+
+The workspace contains two folders by default, where:
+
+- `src`: the folder to maintain sources
+- `lib`: the folder to maintain dependencies
+
+Meanwhile, the compiled output files will be generated in the `bin` folder by default.
---
+++ treeSearch/src/SearchImp.java
@@ -0,0 +1,29 @@
+
+
+import CITS2200.Graph;
+import CITS2200.Search;
+
+/*
+ * @author JoeldotSmith
+ * Tree search implementing breadth first search and depth first search
+ */
+public class SearchImp implements Search {
+    public int[] getConnectedTree(Graph testGraph, int startVertex) {
+        // TODO: Implement getConnectedTree.
+        int a[] = {1, 2};
+        return a;
+    }
+
+    public int[] getDistances(Graph g, int startVertex) {
+        // TODO: Implement getDistances.
+        int a[] = {1, 2};
+        return a;
+    }
+    
+    public int[][] getTimes(Graph g, int startVertex) {
+        // TODO: Implement getTimes.
+        int a[][] = {{1, 2}, {3, 4}};
+        return a;
+        
+    }
+}
